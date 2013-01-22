a1befcfe60e6e3e0be7d8d914e2f261f6ba792b8
Min(Dongmin Yu) <miniway@gmail.com>
2013-01-23 02:16:50 +0900

    fix POLLOUT polling causes InvalidArgumentException

---
+++ src/test/java/org/jeromq/TestZMQ.java
@@ -0,0 +1,60 @@
+package org.jeromq;
+
+import org.jeromq.ZMQ.Context;
+import org.jeromq.ZMQ.Msg;
+import org.jeromq.ZMQ.Socket;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestZMQ
+{
+    static class Client extends Thread {
+
+        private Socket s = null;
+        public Client (Context ctx) {
+            s = ctx.socket(ZMQ.PULL);
+        }
+
+        @Override
+        public void run () {
+            System.out.println("Start client thread ");
+            s.connect( "tcp://127.0.0.1:6669");
+            s.recvMsg(0);
+
+            s.close();
+            System.out.println("Stop client thread ");
+        }
+    }
+
+    @Test
+    public void testPollerPollout () throws Exception
+    {
+        ZMQ.Context context = ZMQ.context(1);
+        Client client = new Client (context);
+
+        //  Socket to send messages to
+        ZMQ.Socket sender = context.socket (ZMQ.PUSH);
+        sender.bind ("tcp://127.0.0.1:6669");
+
+        ZMQ.Poller outItems;
+        outItems = context.poller ();
+        outItems.register (sender, ZMQ.Poller.POLLOUT);
+
+
+        while (!Thread.currentThread ().isInterrupted ()) {
+
+            outItems.poll (1000);
+            if (outItems.pollout (0)) {
+                sender.send ("OK", 0);
+                System.out.println ("ok");
+                break;
+            } else {
+                System.out.println ("not writable");
+                client.start ();
+            }
+        }
+        client.join ();
+        sender.close ();
+        context.term ();
+    }
+}
