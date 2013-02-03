9788a90fd0fc81b56a6c5268602282c8151c9606
John Watson <git@sleazyweasel.com>
2013-02-03 10:29:35 -0800

    hello world examples written for the new API

--- src/main/java/org/jeromq/api/ZeroMQContext.java
+++ src/main/java/org/jeromq/api/ZeroMQContext.java
@@ -5,16 +5,31 @@ import org.zeromq.ZContext;
 public class ZeroMQContext {
     private final ZContext zContext;
 
+    /**
+     * Create a new ZeroMQContext with the specified number of backing threads.
+     */
     public ZeroMQContext(int numberOfThreads) {
         this.zContext = new ZContext(numberOfThreads);
     }
 
+    /**
+     * Create a new ZeroMQContext with a single backing thread.
+     */
     public ZeroMQContext() {
         this(1);
     }
 
+    /**
+     * Create a new socket of the specified type.
+     */
     public Socket createSocket(SocketType type) {
         return new Socket(zContext.createSocket(type.getCValue()));
     }
 
+    /**
+     * Closes all sockets associated with this context and terminates the context itself.
+     */
+    public void terminate() {
+        zContext.destroy();
+    }
 }
---
+++ src/test/java/guide/newapi/HelloWorldClient.java
@@ -0,0 +1,33 @@
+package guide.newapi;
+
+import org.jeromq.api.Socket;
+import org.jeromq.api.SocketType;
+import org.jeromq.api.ZeroMQContext;
+
+/**
+ *  Hello World client in Java
+ *  Connects REQ socket to tcp://localhost:5555
+ *  Sends "Hello" to server, prints the reply ("World")
+ */
+public class HelloWorldClient {
+
+    public static void main(String[] args) {
+        ZeroMQContext context = new ZeroMQContext();
+
+        //  Socket to talk to server
+        System.out.println("Connecting to hello world server");
+
+        Socket socket = context.createSocket(SocketType.REQ);
+        socket.connect("tcp://localhost:5555");
+
+        for (int i = 0; i != 10; i++) {
+            System.out.println("Sending Hello " + i);
+            socket.send("Hello");
+
+            String reply = socket.receiveString();
+            System.out.println("Received " + reply + " " + i);
+        }
+
+        context.terminate();
+    }
+}
