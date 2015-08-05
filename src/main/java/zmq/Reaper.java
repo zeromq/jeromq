914d660b1230a4ad3649f18f85fb14b812bbec35
Scott Banachowski <sbanacho@yahoo.com>
2015-08-05 09:57:40 -0700

    Increment socket count before initiating reap.

--- src/main/java/zmq/Reaper.java
+++ src/main/java/zmq/Reaper.java
@@ -134,10 +134,10 @@ public class Reaper extends ZObject implements IPollEvents, Closeable
     @Override
     protected void processReap(SocketBase socket)
     {
+        ++socketsReaping;
+
         //  Add the socket to the poller.
         socket.startReaping(poller);
-
-        ++socketsReaping;
     }
 
     @Override
