809e5cf2502575a39b82445d3422dd7282e089c3
Dave Yarwood <dave.yarwood@gmail.com>
2016-11-26 14:39:13 -0500

    don't revert this fix; would cause shutdown to hang

--- src/main/java/zmq/Ctx.java
+++ src/main/java/zmq/Ctx.java
@@ -343,7 +343,6 @@ public class Ctx
         try {
             int tid = socket.getTid();
             emptySlots.add(tid);
-            slots[tid].close();
             slots[tid] = null;
 
             //  Remove the socket from the list of sockets.
@@ -354,9 +353,6 @@ public class Ctx
             if (terminating && sockets.isEmpty()) {
                 reaper.stop();
             }
-        }
-        catch (IOException e) {
-            throw new RuntimeException(e);
         }
         finally {
             slotSync.unlock();
