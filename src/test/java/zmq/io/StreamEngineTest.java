c3a3634ee2f409688f6eba9fd5e139d8a9ee2217
fredoboulo <fredoboulo@users.noreply.github.com>
2018-02-25 22:58:55 +0100

    Test for issue #520
    Test for encoder in zero-copy mode with exact buffer size as maximal
    batching size

---
+++ src/test/java/zmq/io/StreamEngineTest.java
@@ -0,0 +1,82 @@
+package zmq.io;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import zmq.Config;
+import zmq.Ctx;
+import zmq.Msg;
+import zmq.SocketBase;
+import zmq.ZMQ;
+import zmq.util.Utils;
+
+public class StreamEngineTest
+{
+    @Test
+    public void testEncoderFlipIssue520() throws IOException
+    {
+        Ctx ctx = ZMQ.createContext();
+        assertThat(ctx, notNullValue());
+
+        SocketBase sender = ZMQ.socket(ctx, ZMQ.ZMQ_PUSH);
+        assertThat(sender, notNullValue());
+
+        boolean rc = ZMQ.setSocketOption(sender, ZMQ.ZMQ_IMMEDIATE, false);
+        assertThat(rc, is(true));
+
+        SocketBase receiver = ZMQ.socket(ctx, ZMQ.ZMQ_PULL);
+        assertThat(receiver, notNullValue());
+
+        final int port = Utils.findOpenPort();
+        final String addr = "tcp://localhost:" + port;
+
+        rc = ZMQ.bind(receiver, addr);
+        assertThat(rc, is(true));
+
+        rc = ZMQ.connect(sender, addr);
+        assertThat(rc, is(true));
+
+        final int headerSize = 8 + 1;
+        // first message + second message header fill the buffer
+        byte[] msg1 = msg(Config.OUT_BATCH_SIZE.getValue() - 2 * headerSize);
+        // second message will go in zero-copy mode
+        byte[] msg2 = msg(Config.OUT_BATCH_SIZE.getValue());
+
+        exchange(sender, receiver, msg1, msg2, msg1, msg2);
+
+        ZMQ.close(receiver);
+        ZMQ.close(sender);
+        ZMQ.term(ctx);
+    }
+
+    private byte[] msg(int length)
+    {
+        byte[] msg = new byte[length];
+        for (int idx = 0; idx < msg.length; ++idx) {
+            msg[idx] = (byte) idx;
+        }
+        return msg;
+    }
+
+    private void exchange(SocketBase sender, SocketBase receiver, byte[]... msgs)
+    {
+        int repetition = 50;
+        for (int idx = 0; idx < repetition; ++idx) {
+            for (byte[] msg : msgs) {
+                int num = ZMQ.send(sender, msg, 0);
+                assertThat(num, is(msg.length));
+            }
+        }
+        for (int idx = 0; idx < repetition; ++idx) {
+            for (byte[] msg : msgs) {
+                Msg received = ZMQ.recv(receiver, 0);
+                assertThat(received.data(), is(msg));
+            }
+        }
+    }
+}
