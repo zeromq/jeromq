bc5fb0040c9c83d0be1dc3e138c53c86682a78ef
fredoboulo <fredoboulo@users.noreply.github.com>
2018-05-18 08:08:15 +0200

    Problem: there is no test for unsubscriptions
    Solution: add a test case based on #554

--- src/test/java/org/zeromq/PubSubTest.java
+++ src/test/java/org/zeromq/PubSubTest.java
@@ -6,8 +6,10 @@ import static org.junit.Assert.assertThat;
 
 import java.io.IOException;
 import java.lang.Thread.UncaughtExceptionHandler;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 
@@ -118,5 +120,97 @@ public class PubSubTest
         pub.close();
         sub.close();
         context.term();
+    }
+
+    @Test
+    public void testUnsubscribeIssue554() throws Exception
+    {
+        final int port = Utils.findOpenPort();
+        final ExecutorService service = Executors.newFixedThreadPool(2);
+
+        final Runnable pub = new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                final ZMQ.Context ctx = ZMQ.context(1);
+                assertThat(ctx, notNullValue());
+                final ZMQ.Socket pub = ctx.socket(ZMQ.PUB);
+                assertThat(pub, notNullValue());
+
+                boolean rc = pub.bind("tcp://*:" + port);
+                assertThat(rc, is(true));
+
+                for (int idx = 1; idx <= 15; ++idx) {
+                    rc = pub.sendMore("test/");
+                    assertThat(rc, is(true));
+                    rc = pub.send("data" + idx);
+                    assertThat(rc, is(true));
+
+                    System.out.printf("Send-%d/", idx);
+                    ZMQ.sleep(1);
+                }
+                pub.close();
+                ctx.close();
+            }
+        };
+        final Callable<Integer> sub = new Callable<Integer>()
+        {
+            @Override
+            public Integer call() throws Exception
+            {
+                final ZMQ.Context ctx = ZMQ.context(1);
+                assertThat(ctx, notNullValue());
+                final ZMQ.Socket sub = ctx.socket(ZMQ.SUB);
+                assertThat(sub, notNullValue());
+
+                boolean rc = sub.setReceiveTimeOut(3000);
+                assertThat(rc, is(true));
+
+                rc = sub.subscribe("test/");
+                assertThat(rc, is(true));
+
+                rc = sub.connect("tcp://localhost:" + port);
+                assertThat(rc, is(true));
+                System.out.println("[SUB]");
+
+                int received = receive(sub, 5);
+                assertThat(received > 1, is(true));
+
+                // unsubscribe from the topic and verify that we don't receive messages anymore
+                rc = sub.unsubscribe("test/");
+                assertThat(rc, is(true));
+                System.out.printf("%n[UNSUB]%n");
+                received = receive(sub, 10);
+
+                sub.close();
+                ctx.close();
+
+                return received;
+            }
+
+            private int receive(ZMQ.Socket socket, int maxSeconds)
+            {
+                int received = 0;
+                long current = System.currentTimeMillis();
+                long end = current + maxSeconds * 1000;
+                while (current < end) {
+                    ZMsg msg = ZMsg.recvMsg(socket);
+                    current = System.currentTimeMillis();
+                    if (msg == null) {
+                        continue;
+                    }
+                    ++received;
+                }
+                return received;
+            }
+        };
+        final Future<Integer> rc = service.submit(sub);
+        service.submit(pub);
+        service.shutdown();
+        service.awaitTermination(60, TimeUnit.SECONDS);
+
+        final int receivedAfterUnsubscription = rc.get();
+        assertThat(receivedAfterUnsubscription, is(0));
     }
 }
