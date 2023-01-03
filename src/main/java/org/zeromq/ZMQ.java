369ebf7abff1c07b5ca8796fb896a6e0ce822711
Fabrice Bacchella <fabrice.bacchella@3ds.com>
2023-01-03 14:49:09 +0100

    With org.zeromq.ZMQ.Event.recv(socket, DONTWAIT), if they is no more event to receive, it throws an NPE altought the javadoc says it should return null. Fixed.
    Also don’t resolve event value for ZMQ_EVENT_MONITOR_STOPPED, it’s a constant.

--- src/main/java/org/zeromq/ZMQ.java
+++ src/main/java/org/zeromq/ZMQ.java
@@ -4260,6 +4260,9 @@ public class ZMQ
         public static Event recv(Socket socket, int flags)
         {
             zmq.ZMQ.Event e = zmq.ZMQ.Event.read(socket.base, flags);
+            if (e == null) {
+                return null;
+            }
             Object resolvedValue;
             switch (e.event) {
             case zmq.ZMQ.ZMQ_EVENT_HANDSHAKE_FAILED_PROTOCOL:
@@ -4280,6 +4283,7 @@ public class ZMQ
                 break;
             case zmq.ZMQ.ZMQ_EVENT_CONNECT_DELAYED:
             case zmq.ZMQ.ZMQ_EVENT_HANDSHAKE_SUCCEEDED:
+            case zmq.ZMQ.ZMQ_EVENT_MONITOR_STOPPED:
                 resolvedValue = null;
                 break;
             default:
